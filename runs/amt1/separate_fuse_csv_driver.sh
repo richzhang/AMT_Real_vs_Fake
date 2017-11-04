14abbfd7ad4af954b7ed3ee65d255c0b64985a27
Richard Zhang <rzhang88@gmail.com>
2017-11-04 01:42:19 -0700

    more runs

--- runs/amt1/separate_fuse_csv_driver.sh
+++ runs/amt1/separate_fuse_csv_driver.sh
@@ -5,16 +5,16 @@ N_ASSIGN=${2}
 # RND_NUMBER=1
 
 # Process preliminary file
-cp ${TURK_ROOT}/${SET_NAME}/results0.csv ${TURK_ROOT}/${SET_NAME}/results0f.csv
-python ./separate_csv_file.py --turk_dir ${TURK_ROOT}/${SET_NAME} --res_filename ./results0f.csv --turk_filename ./${SET_NAME}.csv --N_assign ${N_ASSIGN}
+# cp ${TURK_ROOT}/${SET_NAME}/results0.csv ${TURK_ROOT}/${SET_NAME}/results0f.csv
+# python ./separate_csv_file.py --turk_dir ${TURK_ROOT}/${SET_NAME} --res_filename ./results0f.csv --turk_filename ./${SET_NAME}.csv --N_assign ${N_ASSIGN}
 
-# Process files
-for CUR in 1
+# # Process files
+for CUR in 3
 do
-PREV=$(expr ${CUR} - 1)
-echo '(Previous, Current) results number: ('${PREV}','${CUR}')'
-python ./fuse_csv_files.py --file_dir ${TURK_ROOT}/${SET_NAME} --filenames ./results${PREV}f_pass.csv ./results${CUR}.csv --filename_out results${CUR}f.csv
+	PREV=$(expr ${CUR} - 1)
+	echo '(Previous, Current) results number: ('${PREV}','${CUR}')'
+	python ./fuse_csv_files.py --file_dir ${TURK_ROOT}/${SET_NAME} --filenames ./results${PREV}f_pass.csv ./results${CUR}.csv --filename_out results${CUR}f.csv
 
-# Process 1f
-python ./separate_csv_file.py --turk_dir ${TURK_ROOT}/${SET_NAME} --res_filename ./results${CUR}f.csv --turk_filename ./${SET_NAME}.csv --N_assign ${N_ASSIGN}
+	# Process 1f
+	python ./separate_csv_file.py --turk_dir ${TURK_ROOT}/${SET_NAME} --res_filename ./results${CUR}f.csv --turk_filename ./${SET_NAME}.csv --N_assign ${N_ASSIGN}
 done
